bb5bb5d54d555b7fa259d2ff0ebf62d86bb2a122
Reza Jelveh <toyb0mb@gmx.net>
2010-10-10 23:10:11 +0200

    ok

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,15 @@
 class OrdersController < ApplicationController
+  def express
+    response = EXPRESS_GATEWAY.setup_purchase(
+      Order.new.price_in_cents,
+      :ip                => request.remote_ip,
+      :return_url        => new_order_url,
+      :cancel_return_url => products_url
+    )
+    redirect_to EXPRESS_GATEWAY.redirect_url_for(response.token)
+  end
+
+  def new
+    @order = Order.new(:express_token => params[:token])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
   map.root :controller => :products
 
-  map.resources :orders
+  map.resources :products
+  map.resources :orders, :new => { :express => :get }
 
   # consider removing or commenting them out if you're using named routes and resources.
   map.connect ':controller/:action/:id'
