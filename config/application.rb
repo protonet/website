08786f6229dbf01ef01761905b33771cae26ed4a
Daniel Lamando <me@danopia.net>
2011-10-21 00:04:14 -0400

    Work on moving to pipelines, for the heck of it

--- config/application.rb
+++ config/application.rb
@@ -44,5 +44,8 @@ module ProtonetHomepage
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    
+    # Add a fonts pipeline
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
