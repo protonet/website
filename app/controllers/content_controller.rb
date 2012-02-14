d4348da8730fcd22e1ff56b37f8d9fd6025cabf8
wpeters01 <peterswolfgang@web.de>
2012-02-14 15:38:25 +0100

    Update app/controllers/content_controller.rb

--- app/controllers/content_controller.rb
+++ app/controllers/content_controller.rb
@@ -16,31 +16,31 @@ class ContentController < ApplicationController
       {
         :name     => "Ali Jelveh",
         :picture  => "uploads/pics/dude_1.jpg",
-        :position => "GF",
-        :description => "Lorem ipsum dolor sit amet lorem ipsum dolor sit. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, takimata sanctus est."
+        :position => "Founder/ CEO",
+        :description => "Ali joined the XING AG as a RoR developerwhere he decided to start working on the idea protonet. When not working, Ali spends his time thinking, planning and simulating."
       },
       {
         :name => "Christopher Blum",
         :picture => "uploads/pics/dude_2.jpg",
-        :position => "GF",
-        :description => "Lorem ipsum dolor sit amet lorem ipsum dolor sit. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, takimata sanctus est."
+        :position => "Founder/ CEO",
+        :description => "Christopher founded his first company during school, studied communication design and got to know Ali and protonet at XING. In his free time he enjoys football or beers with friends."
       },
       {
-        :name => "Martin Huegli",
+        :name => "Henning Thies",
+        :picture => "uploads/pics/dude_3.jpg",
+        :position => "Developer",
+        :description => "Henning established several web projects, worked for programming agencies and studied IT and Design. He met Christopher and Ali at the betahaus Hamburg."
+      },
+      {
+        :name => "Wolfgang Peters",
+        :picture => "uploads/pics/dude_4.jpg",
+        :position => "Business",
+        :description => "Wolfgang studied business administration, contributed to several startup projects and worked for the incubator Hanse Ventures. He met protonet at the betahaus."
+      },
+      {
+        :name => "Johannes von Bargen",
         :picture => "uploads/pics/dude_5.jpg",
-        :position => "GF",
-        :description => "Lorem ipsum dolor sit amet lorem ipsum dolor sit. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, takimata sanctus est."
-      },
-      {
-        :name => "David Burkhardt",
-        :picture => "uploads/pics/dude_5_01.jpg",
-        :position => "GF",
-        :description => "Lorem ipsum dolor sit amet lorem ipsum dolor sit. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, takimata sanctus est."
-      },
-      {
-        :name => "NA",
-        :picture => "uploads/pics/dude_5_02.jpg",
-        :position => "GF",
+        :position => "Scientist",
         :description => "Lorem ipsum dolor sit amet lorem ipsum dolor sit. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, takimata sanctus est."
       },
       {
